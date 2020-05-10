8663d5edd045431e6e1745ea65d35d7eb8b10a8d
Paulo ES <pauloeduardo.dg@outlook.com>
2020-05-10 01:15:44 -0300

    Views

--- miniframework/App/Controllers/indexController.php
+++ miniframework/App/Controllers/indexController.php
@@ -2,14 +2,28 @@
 namespace App\Controllers;
 
 class IndexController {
+
+    private $view;
+
+    public function __construct(){
+        $this->view = new \stdClass();
+    }
+
     public function index(){
-        echo "chegamos ao index controller e disparamos a action index";
+        $this->view->dados = array('sofa','casa','cadeado');
+        $this->render('index');
     }
     public function sobreNos(){
-        echo "chegamos ao index controller e disparamos a action sobreNos";
+        $this->view->dados = array('note','mouse','cadeado');
+        $this->render('sobreNos');
+    }
+    public function render($view){
+        $classAtual = get_class($this);
+        $classAtual = str_replace('App\\Controllers\\', '', $classAtual);
+        $classAtual = strtolower(str_replace('Controller', '', $classAtual));
 
+        require_once "../App/Views/".$classAtual."/".$view.".phtml";
     }
 }
-
 
 ?>
