1e7178ccd7e582b5caf39728a7e7e61761000287
Paulo ES <pauloeduardo.dg@outlook.com>
2020-05-10 17:14:04 -0300

    Conexão com Banco de dados e Models

--- miniframework/App/Controllers/indexController.php
+++ miniframework/App/Controllers/indexController.php
@@ -1,15 +1,28 @@
 <?php
 namespace App\Controllers;
 use MF\Controller\Action;
+use App\Connection;
+use App\Models\Produto;
 
 class IndexController extends Action {
 
     public function index(){
-        $this->view->dados = array('sofa','casa','cadeado');
+      //  $this->view->dados = array('sofa','casa','cadeado');
+
+        $conn = Connection::getDb();
+
+        $produto = new Produto($conn);
+
+        $produtos = $produto->getProdutos();
+
+        $this->view->dados = $produtos;
+
         $this->render('index', 'layout1');
+
     }
     public function sobreNos(){
-        $this->view->dados = array('note','mouse','cadeado');
+      // $this->view->dados = array('note','mouse','cadeado');
+
         $this->render('sobreNos', 'layout2');
     }
 }
